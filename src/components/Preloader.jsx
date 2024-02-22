d504ec564b138e11c5e09204ec9d192e6bd920f0
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-22 23:13:25 +0530

    Added Changes

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
 
 const Preloader = () => {
+  const container = useRef(null);
+
+  useLayoutEffect(() => {
+    let context = gsap.context(() => {
+      const timeline = gsap.timeline();
+
+      //   timeline.to("#sample", {
+      //     background: "red",
+      //     duration: 2,
+      //     delay: 1,
+      //   });
+    }, container);
+
+    return () => context.revert();
+  }, []);
+
   return (
-    <div className="absolute left-[500px] h-screen w-screen border bg-black bg-opacity-80">
-      Preloader
+    <div
+      ref={container}
+      className="absolute flex h-screen w-screen items-center justify-center border-2 bg-black bg-opacity-80 font-spaceMono text-5xl font-black uppercase text-white"
+    >
+      <p
+        id="sample"
+        className="bg-purple-500"
+        onMouseEnter={() => {
+          gsap.to("#sample", {
+            background: "red",
+          });
+        }}
+        onMouseLeave={() => {
+          gsap.to("#sample", {
+            background: "#a855f7",
+          });
+        }}
+      >
+        Preloader
+      </p>
     </div>
   );
 };
