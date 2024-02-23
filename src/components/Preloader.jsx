7eba152f3d36a033f7528084c8ec6f565654c129
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-23 12:48:17 +0530

    Added Changes

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,43 +1,71 @@
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 
 const Preloader = () => {
   const container = useRef(null);
+  const counter = useRef(null);
 
   useLayoutEffect(() => {
     let context = gsap.context(() => {
       const timeline = gsap.timeline();
 
-      //   timeline.to("#sample", {
-      //     background: "red",
-      //     duration: 2,
-      //     delay: 1,
-      //   });
+      timeline.to("#character", {
+        background: "red",
+        duration: 2,
+        delay: 1,
+      });
     }, container);
 
     return () => context.revert();
   }, []);
 
+  useEffect(() => {
+    const startLoader = () => {
+      let currentValue = 0;
+
+      const updateCounter = () => {
+        if (currentValue === 100) return;
+
+        currentValue += Math.floor(Math.random() * 10) + 1;
+
+        if (currentValue > 100) {
+          currentValue = 100;
+        }
+
+        console.log(currentValue + "%");
+
+        counter.current.textContent = currentValue + "%";
+
+        let delay = Math.floor(Math.random() * 200) + 250;
+        setTimeout(updateCounter, delay);
+      };
+
+      updateCounter();
+    };
+
+    startLoader();
+  }, []);
+
   return (
     <div
       ref={container}
-      className="absolute flex h-screen w-screen items-center justify-center border-2 bg-black bg-opacity-80 font-spaceMono text-5xl font-black uppercase text-white"
+      className="absolute flex h-screen w-screen items-center justify-center bg-red-950 bg-opacity-80 font-spaceMono"
     >
       <p
         id="sample"
-        className="bg-purple-500"
-        onMouseEnter={() => {
-          gsap.to("#sample", {
-            background: "red",
-          });
-        }}
-        onMouseLeave={() => {
-          gsap.to("#sample", {
-            background: "#a855f7",
-          });
-        }}
+        className="font-honk border px-2 py-2 text-5xl font-black uppercase text-white"
       >
-        Preloader
+        {"Preloader".split("").map((character, index) => (
+          <span key={index} id="character" className="">
+            {character}
+          </span>
+        ))}
+      </p>
+      <p
+        ref={counter}
+        className="absolute bottom-20 right-20 text-9xl font-black uppercase text-white"
+      >
+        0
       </p>
     </div>
   );
